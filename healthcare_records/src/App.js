1aed6c493f76d479ef54e286b22867b98505e4ff
Georgios Taskos <george@xplatsolutions.com>
2019-02-23 15:22:33 -0500

    Handle value change for username and password inputs. Sign in button function  with mock data workflow.

--- healthcare_records/src/App.js
+++ healthcare_records/src/App.js
@@ -1,61 +1,55 @@
-import React, { Component } from 'react';
-import './App.css';
-import SearchPatients from './components/doctors/SearchPatients';
-import UserSignIn from './components/user_sign_in/UserSignIn';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from "react";
+import "./App.css";
+import UserSignIn from "./components/user_sign_in/UserSignIn";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
 
 class App extends Component {
-
   state = {
     patients: [
       {
-      id: 1,
-      name: "Anakin Skywalker",
-      age: 38,
-      emailAddress: "anakin@tempus.com",
-      mailingAddress: "Somewhere in Galaxy",
-      phoneNumber: "8009001000"
-    },
-    {
-      id: 2,
-      name: "Master Yoda",
-      age: 38,
-      emailAddress: "yoda@tempus.com",
-      mailingAddress: "Somewhere in Galaxy",
-      phoneNumber: "8009001000"
-    }
-  ]
-  }
-
-  patientClicked = (id) => {
-    console.log(id)
-  }
+        id: 1,
+        name: "Anakin Skywalker",
+        age: 38,
+        emailAddress: "anakin@tempus.com",
+        mailingAddress: "Somewhere in Galaxy",
+        phoneNumber: "8009001000"
+      },
+      {
+        id: 2,
+        name: "Master Yoda",
+        age: 38,
+        emailAddress: "yoda@tempus.com",
+        mailingAddress: "Somewhere in Galaxy",
+        phoneNumber: "8009001000"
+      }
+    ]
+  };
 
   render() {
     return (
       <div className="App">
-        {/* <SearchPatients patients={this.state.patients} patientClicked={this.patientClicked} /> */}
-        
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              Tempus
-          </Typography>
-          </Toolbar>
-        </AppBar>
-        <UserSignIn/>
+        <MuiThemeProvider theme={theme}>
+          {/* <SearchPatients patients={this.state.patients} patientClicked={this.patientClicked} /> */}
+
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography color="inherit">Tempus</Typography>
+            </Toolbar>
+          </AppBar>
+          <UserSignIn />
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
--- healthcare_records/src/components/user_sign_in/UserSignIn.js
+++ healthcare_records/src/components/user_sign_in/UserSignIn.js
@@ -1,59 +1,90 @@
-import React, { Component, Fragment } from 'react';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import Appbar from '@material-ui/core/AppBar';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { Component, Fragment } from "react";
+import Appbar from "@material-ui/core/AppBar";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export class UserSignIn extends Component {
+  state = {
+    username: "",
+    password: "",
+    users: [
+      {
+        username: "anakin",
+        password: "george",
+        role: "patient"
+      },
+      {
+        username: "yoda",
+        password: "george",
+        role: "doctor"
+      }
+    ]
+  };
 
-    state = {
-        users: [
-            {
-                username: 'anakin',
-                password: '',
-                role: 'patient'
-            },
-            {
-                username: 'yoda',
-                password: '',
-                role: 'doctor'
-            }]
-    }
+  handleChange = input => e => {
+      this.setState({[input]: e.target.value});
+  }
 
-    userSignIn = e => {
-        e.PreventDefault()
+  userSignIn = e => {
+    //e.PreventDefault();
+
+    const { users, username, password } = this.state;
+
+    const signInUser = users.filter(user => {
+      return user.username === username && user.password === password;
+    });
+
+    if (signInUser.length > 0) {
+      const user = signInUser[0];
+      // move to search patients if doctor or patient details of patient
+      if (user.role === "doctor") {
+        // route to search patient
+        console.log("route to search patient");
+      }
+
+      if (user.role === "patient") {
+        // route to patient details
+        console.log("route to patient details");
+      }
+    } else {
+      console.log("no login");
     }
+  };
 
   render() {
     return (
-      <MuiThemeProvider>
-        <Fragment>
-            <Appbar title="Sign In" />
-            <br />
-                <TextField
-                    id="standard-username"
-                    label="Username"
-                    value={this.state.name}
-                    margin="normal"
-                />
-                <br />
-            <TextField id="standard-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    margin="normal"
-                />
-                <br /><br /><br />
-                <Button variant="contained" 
-                    size="medium" 
-                    color="primary" >
-                    Sign In
+      <Fragment>
+        <Appbar title="Sign In" />
+        <br />
+        <TextField 
+            id="standard-username" 
+            label="Username" 
+            margin="normal"
+            onChange={this.handleChange('username')}
+         />
+        <br />
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          onChange={this.handleChange('password')}
+        />
+        <br />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          size="medium"
+          color="primary"
+          onClick={this.userSignIn}
+        >
+          Sign In
         </Button>
-
-        </Fragment>
-      </MuiThemeProvider>
-    )
+      </Fragment>
+    );
   }
 }
 
-export default UserSignIn
+export default UserSignIn;
